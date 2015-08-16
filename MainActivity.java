e7e2c9bfdeac02c896688df06a7fee7f9efcdee1
zkoticha <zkoticha@gmail.com>
2015-08-15 22:31:20 -0400

    Video Stuff removed

--- MainActivity.java
+++ MainActivity.java
@@ -2,39 +2,19 @@ package on1y.example.org.on1y;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
 import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.VideoView;
-
-
-
-import android.content.res.Configuration;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnPreparedListener;
-
-
-import com.google.android.youtube.player.YouTubePlayer;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -79,60 +59,6 @@ public class MainActivity extends AppCompatActivity
                     .commit();
         }
 
-        //set the media controller buttons
-        if (mediaControls == null) {
-            mediaControls = new MediaController(MainActivity.this);
-        }
-
-        //initialize the VideoView
-        myVideoView = (VideoView) findViewById(R.id.main_video);
-
-        // create a progress bar while the video file is loading
-        progressDialog = new ProgressDialog(MainActivity.this);
-        // set a title for the progress bar
-        progressDialog.setTitle("JavaCodeGeeks Android Video View Example");
-        // set a message for the progress bar
-        progressDialog.setMessage("Loading...");
-        //set the progress bar not cancelable on users' touch
-        progressDialog.setCancelable(false);
-        // show the progress bar
-        progressDialog.show();
-
-        try {
-            //set the media controller in the VideoView
-            mediaControls.setAnchorView(myVideoView);
-
-        } catch (Exception e) {
-            Log.e("Error98", "on1y");
-            e.printStackTrace();
-        }
-
-        try {
-            //set the uri of the video to be played
-            myVideoView.setVideoPath("https://www.youtube.com/watch?v=F4QzhSlqmqg");
-        } catch (Exception e) {
-            Log.e("Error99", "on1y");
-            e.printStackTrace();
-        }
-
-
-        myVideoView.requestFocus();
-        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
-        myVideoView.setOnPreparedListener(new OnPreparedListener() {
-
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                // close the progress bar and play the video
-                progressDialog.dismiss();
-                //if we have a position on savedInstanceState, the video playback should start from here
-                myVideoView.seekTo(pos);
-                if (pos == 0) {
-                    myVideoView.start();
-                } else {
-                    //if we come from a resumed activity, video playback will be paused
-                    myVideoView.pause();
-                }
-            }
-        });
 
     }
 
@@ -226,6 +152,8 @@ public class MainActivity extends AppCompatActivity
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+
+
         }
 
         @Override

a6b1f301dfd56f97db042b9fa3bfdb7de1700b04
zkoticha <zkoticha@gmail.com>
2015-08-17 22:05:59 -0400

    Images clickable.

--- BusinessFragment.java
+++ BusinessFragment.java
@@ -1,10 +1,14 @@
 package on1y.example.org.on1y;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 /**
  * Created by manveenkoticha on 8/17/15.
  */
@@ -14,13 +18,13 @@ public class BusinessFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_business, container, false);
         //make preview clickable
-//        ImageView imageView = (ImageView) rootView.findViewById(R.id.science_preview);
-    /*    imageView.setOnClickListener(new View.OnClickListener() {
+       ImageView imageView = (ImageView) rootView.findViewById(R.id.business_preview);
+       imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=F4QzhSlqmqg")));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?t=10&v=ekWMIndt6cA")));
             }
-        }); */
+        });
         return rootView;
     }
     public BusinessFragment() {
